991fa0d0040c0384947ca35bb1531b0225744564
Yushi Wang <jasper.ys.wang@gmail.com>
2016-02-17 18:10:37 -0700

    implement apis

--- server.py
+++ server.py
@@ -74,28 +74,31 @@ def flask_post_json():
 
 @app.route("/")
 def hello():
-    '''Return something coherent here.. perhaps redirect to /static/index.html '''
     return redirect('/static/index.html')
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
-    '''update the entities via this interface'''
-    return None
+    if request.method == 'POST':
+        return myWorld.set(entity, flask_post_json())
+    else:
+        return get_entity(entity)
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
-    '''you should probably return the world here'''
-    return None
+    if request.method == 'GET':
+        return json.dumps(myWorld.world())
+    else:
+        return None
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
-    '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return json.dumps(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return  json.dumps(None)
 
 if __name__ == "__main__":
     app.run()
